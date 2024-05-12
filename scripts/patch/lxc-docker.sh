df06ccf742505bc3bd019e6914192b2bf4b7d4cb
thoryo <53646847+miaoyehao@users.noreply.github.com>
2024-05-12 23:20:00 +0800

    Update lxc-docker.sh

--- scripts/patch/lxc-docker.sh
+++ scripts/patch/lxc-docker.sh
@@ -6,8 +6,8 @@ patchf(){
     fi
 }
 
-# patchf kernel/cgroup.c $LXC_PATCHES/cgroup.patch
+patchf kernel/cgroup.c $LXC_PATCHES/cgroup.patch
 patchf kernel/cgroup/cgroup.c $LXC_PATCHES/cgroup.patch
 patchf net/netfilter/xt_qtaguid.c $LXC_PATCHES/xt_qtaguid.patch
-# patchf include/linux/mm.h $LXC_PATCHES/mm.patch
+patchf include/linux/mm.h $LXC_PATCHES/mm.patch
 patchf kernel/cgroup/cpuset.c $LXC_PATCHES/cpuset.patch
